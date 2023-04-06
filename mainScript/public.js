0989c8073733795bec738dbcffc85723d41a22ee
Horizon719 <milichovszky.sandor@gmail.com>
2023-04-06 18:51:07 +0200

    public.htmlben a kárlyák létrehozása, navbar és header modositas

--- mainScript/public.js
+++ mainScript/public.js
@@ -1,3 +1,23 @@
+import { 
+    OBJECTS,
+} from "../datas/public.js";
+
 $(function(){
-    
+    const ARTICLE = $("article");
+    ARTICLE.eq(0).html(``);
+    let txt = feltolt(OBJECTS);
+    ARTICLE.eq(0).html(txt);
 })
+
+function feltolt(OBJECTS){
+    let text = `<div class="container mt-3 row">`;
+    for (let i = 0; i < OBJECTS.length; i++) {
+        text += `<div class="card col-sm-3">`;
+        text += `<div class="card-header">${OBJECTS[i].nev}<br>${OBJECTS[i].kategoria}</div>`;
+        text += `<div class="card-body"><img src="${OBJECTS[i].eleres}" alt="${OBJECTS[i].kategoria}"></div> `;
+        text += `<div class="card-footer">${OBJECTS[i].ar} HUF</div>`;
+        text += `</div>`;
+    }
+    text += "</div>";
+    return text;
+}
