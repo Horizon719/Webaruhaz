e38fc9426c29d8ee513aa97edc8cadc74e6f0c40
Horizon719 <milichovszky.sandor@gmail.com>
2023-04-07 10:48:32 +0200

    Kis formázás, public.html-en kártyákra gombok, random sorrend és a html fileok kiszedése mappából

--- mainScript/public.js
+++ mainScript/public.js
@@ -3,6 +3,7 @@ import {
 } from "../datas/public.js";
 
 $(function(){
+    kever(OBJECTS);
     const ARTICLE = $("article");
     ARTICLE.eq(0).html(``);
     let txt = feltolt(OBJECTS);
@@ -13,11 +14,22 @@ function feltolt(OBJECTS){
     let text = `<div class="container mt-3 row">`;
     for (let i = 0; i < OBJECTS.length; i++) {
         text += `<div class="card col-sm-3">`;
-        text += `<div class="card-header">${OBJECTS[i].nev}<br>${OBJECTS[i].kategoria}</div>`;
+        text += `<div class="card-header"><h4>${OBJECTS[i].nev}</h4><br>-${OBJECTS[i].kategoria}</div>`;
         text += `<div class="card-body"><img src="${OBJECTS[i].eleres}" alt="${OBJECTS[i].kategoria}"></div> `;
-        text += `<div class="card-footer">${OBJECTS[i].ar} HUF</div>`;
+        text += `<div class="card-footer">${OBJECTS[i].ar} HUF<br><button>Mutat</button><button>Kosárba</button></div>`;
         text += `</div>`;
     }
     text += "</div>";
     return text;
+}
+
+function kever(OBJECTS){
+    for (let i = 0; i < 100; i++) {
+        let indEgy = Math.floor(Math.random() * OBJECTS.length);
+        let indKetto = Math.floor(Math.random() * OBJECTS.length);
+        let z = "";
+        z = OBJECTS[indEgy];
+        OBJECTS[indEgy] = OBJECTS[indKetto];
+        OBJECTS[indKetto] = z;
+    }
 }
