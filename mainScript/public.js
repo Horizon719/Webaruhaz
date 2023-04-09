e6330176f96a2adb65d62f0333414df08860c51b
Horizon719 <milichovszky.sandor@gmail.com>
2023-04-09 12:22:18 +0200

    A mutat gombra megjelenik az adott termék képe, a következő és előző gombokra a megfelelő termékre vált

--- mainScript/public.js
+++ mainScript/public.js
@@ -3,11 +3,33 @@ import {
 } from "../datas/public.js";
 
 $(function(){
+    let currentIndex;
     kever(OBJECTS);
     const ARTICLE = $("article");
     ARTICLE.eq(0).html(``);
     let txt = feltolt(OBJECTS);
     ARTICLE.eq(0).html(txt);
+
+    $(".show").on("click", function (event){
+        currentIndex = parseInt($(event.target).attr("id"));
+        ARTICLE.eq(0).html(`<div id="nagykep"><button id="elozo">Előző termék</button>
+                            <div class="nagykep"><img src="${OBJECTS[currentIndex].eleres}" alt=""></div>
+                            <button id="kovetkezo">Következő termék</button></div>`);
+        
+        const NAGYKEP = $(".nagykep img").eq(0);
+        const ELOZO = $("#elozo").eq(0);
+        ELOZO.on("click", function () {
+            currentIndex = leptetes(-1, currentIndex);
+            NAGYKEP.attr("src", OBJECTS[currentIndex].eleres);
+        });
+        const KOVETKEZO = $("#kovetkezo").eq(0);
+        KOVETKEZO.on("click", function () {
+            currentIndex = leptetes(+1, currentIndex);
+            NAGYKEP.attr("src", OBJECTS[currentIndex].eleres);
+        });
+    });
+    
+    
 })
 
 function feltolt(OBJECTS){
@@ -16,7 +38,7 @@ function feltolt(OBJECTS){
         text += `<div class="card col-sm-3">`;
         text += `<div class="card-header"><h4>${OBJECTS[i].nev}</h4><br>-${OBJECTS[i].kategoria}</div>`;
         text += `<div class="card-body"><img src="${OBJECTS[i].eleres}" alt="${OBJECTS[i].kategoria}"></div> `;
-        text += `<div class="card-footer">${OBJECTS[i].ar} HUF<br><button>Mutat</button><button>Kosárba</button></div>`;
+        text += `<div class="card-footer">${OBJECTS[i].ar} HUF<br><button class="show" id="${i}">Mutat</button><button>Kosárba</button></div>`;
         text += `</div>`;
     }
     text += "</div>";
@@ -32,4 +54,15 @@ function kever(OBJECTS){
         OBJECTS[indEgy] = OBJECTS[indKetto];
         OBJECTS[indKetto] = z;
     }
+}
+
+function leptetes(ertek, currentIndex){
+    currentIndex += ertek;
+    if (currentIndex > OBJECTS.length - 1) {
+      currentIndex = 0;
+    }
+    if (currentIndex < 0) {
+      currentIndex = OBJECTS.length - 1;
+    }
+    return currentIndex;
 }
