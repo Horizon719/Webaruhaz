a7bd7c8b7b4581ec2708196f68bc34a592ddc946
PetiDominik <petidomi05@gmail.com>
2023-04-11 13:20:43 +0200

    rossz import javitas

---
+++ js/datas.js
@@ -0,0 +1,78 @@
+export const OBJECTS =  [
+    {
+        nev: "Gamer billentyűzet", 
+        kategoria: "billentyűzet",
+        ar: 16000,
+        eleres: "../kepek/keyboard1.png",
+    },
+    {
+        nev: "Félbillentyűzet", 
+        kategoria: "billentyűzet", 
+        ar: 10000,
+        eleres: "../kepek/keyboard2.png",
+    },
+    {
+        nev: "Szilikon billentyűzet", 
+        kategoria: "billentyűzet", 
+        ar: 30000,
+        eleres: "../kepek/keyboard3.png",
+    },
+    {
+        nev: "Hajlított monitor", 
+        kategoria: "monitor", 
+        ar: 56000,
+        eleres: "../kepek/monitor1.jpg",
+    },
+    {
+        nev: "Multi screen monitor", 
+        kategoria: "monitor", 
+        ar: 50000,
+        eleres: "../kepek/monitor2.jpg",
+    },
+    {
+        nev: "Egyszerű monitor", 
+        kategoria: "monitor", 
+        ar: 40000,
+        eleres: "../kepek/monitor3.png",
+    },
+    {
+        nev: "Vezetékes egér", 
+        kategoria: "egér", 
+        ar: 12000,
+        eleres: "../kepek/mouse1.png",
+    },
+    {
+        nev: "Lyukacsos egér", 
+        kategoria: "egér", 
+        ar: 10000,
+        eleres: "../kepek/mouse2.jpg",
+    },
+    {
+        nev: "Vezeték nélküli egér", 
+        kategoria: "egér", 
+        ar: 20000,
+        eleres: "../kepek/mouse3.jpg",
+    },
+    {
+        nev: "Komplett pc", 
+        kategoria: "pc", 
+        ar: 500000,
+        eleres: "../kepek/pc1.jpg",
+    },
+    {
+        nev: "Gamer pc", 
+        kategoria: "pc", 
+        ar: 750000,
+        eleres: "../kepek/pc2.jpg",
+    },
+    {
+        nev: "Laptop", 
+        kategoria: "laptop", 
+        ar: 250000,
+        eleres: "../kepek/laptop.jpg",
+    },
+];
+
+export const KOSARELEMEI =  [
+
+];
